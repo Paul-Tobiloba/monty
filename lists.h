83722a99874c51ca98873c667af955c2e1862a82
Paul Tobiloba <48536008+Toby-Codes@users.noreply.github.com>
2021-09-23 23:23:09 +0100

    [Add] function lists

--- lists.h
+++ lists.h
@@ -3,14 +3,14 @@
 
 #include "monty.h"
 
-/* list_funcs1.c */
+/* 0-list_funcs.c */
 size_t dlistint_len(const dlistint_t *h);
 dlistint_t *add_dnodeint(dlistint_t **head, const int n);
 size_t print_dlistint(const dlistint_t *h);
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index);
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index);
 
-/* list_funcs2.c */
+/*1- list_funcs.c */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n);
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n);
 void free_dlistint(dlistint_t *head);
